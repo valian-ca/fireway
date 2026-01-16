a5b08bbdda3fb9d9849e7a486e84f6ccf4f49fcf
Julien Marcil <julien@valian.ca>
2026-01-15 20:33:02 -0500

    chore: update deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import { importSort } from '@valian/eslint-config/import-sort'
 import { json } from '@valian/eslint-config/json'
 import { typescript } from '@valian/eslint-config/typescript'
 import { vitest } from '@valian/eslint-config/vitest'
+import { zod } from '@valian/eslint-config/zod'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -11,6 +12,7 @@ export default defineConfig([
   ...importSort,
   ...json,
   ...vitest,
+  ...zod,
   globalIgnores(['**/lib', '**/coverage/']),
   {
     files: ['**/__tests__/**'],
