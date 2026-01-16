1c8875ed0831ea47a248b775aea6944d72c1b66b
Julien Marcil <julien@valian.ca>
2026-01-15 20:30:16 -0500

    chore: update project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,6 @@ import { importSort } from '@valian/eslint-config/import-sort'
 import { json } from '@valian/eslint-config/json'
 import { typescript } from '@valian/eslint-config/typescript'
 import { vitest } from '@valian/eslint-config/vitest'
-import { zod } from '@valian/eslint-config/zod'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -12,13 +11,14 @@ export default defineConfig([
   ...importSort,
   ...json,
   ...vitest,
-  ...zod,
   globalIgnores(['**/lib', '**/coverage/']),
   {
     files: ['**/__tests__/**'],
     rules: {
       'unicorn/filename-case': 'off',
       'unicorn/no-empty-file': 'off',
+
+      'vitest/no-conditional-expect': 'off',
     },
   },
 ])
